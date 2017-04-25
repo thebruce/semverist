94b1bcb9715aa4662e50a904a97193a89cf49b67
David Diers <david.diers@fourkitchens.com>
2017-04-24 21:30:09 -0500

    fix(adjacency): Non adjacent named prereleases will no longer report as adjacent.:

--- test/semverish/range/semverAdjacent.js
+++ test/semverish/range/semverAdjacent.js
@@ -122,6 +122,21 @@ test('Semverish76ParentToChild', async (t) => {
   );
 });
 
+test('adjacentPrereleasesDifferentGreek', async (t) => {
+  t.context.data = await rangeFactory('semverist', 'range')
+  .then((RangeClass) => {
+    const rangeClass = new RangeClass();
+    rangeClass.init();
+    rangeClass.setSemveristElementType('entity');
+    rangeClass.setOptions();
+    return rangeClass.analyzeSemverishAdjacency('1.0.0-alpha.0', '1.0.0-beta.1');
+  });
+  t.false(
+    t.context.data.adjacent,
+    'A child path of a parent should return as a child when semverish values are compared.'
+  );
+});
+
 
 test('Semverish5ParentToChild', async (t) => {
   t.context.data = await rangeFactory('semverist', 'range')
